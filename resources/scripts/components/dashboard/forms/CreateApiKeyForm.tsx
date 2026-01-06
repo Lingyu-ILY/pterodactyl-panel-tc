011e218601f6d4cc1df4105d88d6568cdbbab49d
Lingyu-ILY <lingyu2022352@gmail.com>
2026-01-06 18:02:42 +0800

    chore(i18n): translate dashboard/forms strings to zh-TW

--- resources/scripts/components/dashboard/forms/CreateApiKeyForm.tsx
+++ resources/scripts/components/dashboard/forms/CreateApiKeyForm.tsx
@@ -59,24 +59,24 @@ export default ({ onKeyCreated }: { onKeyCreated: (key: ApiKey) => void }) => {
                     <Form>
                         <SpinnerOverlay visible={isSubmitting} />
                         <FormikFieldWrapper
-                            label={'Description'}
+                            label={'說明'}
                             name={'description'}
-                            description={'A description of this API key.'}
+                            description={'此 API 金鑰的說明。'}
                             css={tw`mb-6`}
                         >
                             <Field name={'description'} as={Input} />
                         </FormikFieldWrapper>
                         <FormikFieldWrapper
-                            label={'Allowed IPs'}
+                            label={'允許的 IP'}
                             name={'allowedIps'}
                             description={
-                                'Leave blank to allow any IP address to use this API key, otherwise provide each IP address on a new line.'
+                                '留空代表允許任何 IP 位址使用此 API 金鑰；否則請每行輸入一個 IP 位址。'
                             }
                         >
                             <Field name={'allowedIps'} as={CustomTextarea} />
                         </FormikFieldWrapper>
                         <div css={tw`flex justify-end mt-6`}>
-                            <Button>Create</Button>
+                            <Button>建立</Button>
                         </div>
                     </Form>
                 )}
--- resources/scripts/components/dashboard/forms/RecoveryTokensDialog.tsx
+++ resources/scripts/components/dashboard/forms/RecoveryTokensDialog.tsx
@@ -20,9 +20,9 @@ export default ({ tokens, open, onClose }: RecoveryTokenDialogProps) => {
         <Dialog
             open={open}
             onClose={onClose}
-            title={'Two-Step Authentication Enabled'}
+            title={'已啟用兩步驟驗證'}
             description={
-                'Store the codes below somewhere safe. If you lose access to your phone you can use these backup codes to sign in.'
+                '請將以下代碼存放在安全的位置。若你無法存取手機，可用這些備用代碼登入。'
             }
             hideCloseIcon
             preventExternalClose
@@ -41,10 +41,10 @@ export default ({ tokens, open, onClose }: RecoveryTokenDialogProps) => {
                 </pre>
             </CopyOnClick>
             <Alert type={'danger'} className={'mt-3'}>
-                These codes will not be shown again.
+                這些代碼不會再次顯示。
             </Alert>
             <Dialog.Footer>
-                <Button.Text onClick={onClose}>Done</Button.Text>
+                <Button.Text onClick={onClose}>完成</Button.Text>
             </Dialog.Footer>
         </Dialog>
     );
