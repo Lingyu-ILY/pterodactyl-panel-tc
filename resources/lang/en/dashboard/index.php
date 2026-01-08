e1c02cbae16074bb253d0ae7b4a7e2204be84314
Lingyu-ILY <lingyu2022352@gmail.com>
2026-01-08 16:49:18 +0800

    chore(tw): translate dashboard/index language strings to Traditional Chinese

--- resources/lang/en/dashboard/index.php
+++ resources/lang/en/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Search for servers...',
-    'no_matches' => 'There were no servers found matching the search criteria provided.',
+    'search' => '搜尋伺服器...',
+    'no_matches' => '沒有找到與搜尋條件相符的伺服器。',
     'cpu_title' => 'CPU',
-    'memory_title' => 'Memory',
+    'memory_title' => '記憶體',
 ];
