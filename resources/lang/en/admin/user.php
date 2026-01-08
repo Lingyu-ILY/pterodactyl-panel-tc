5a0a9a9c1b5f4b13f53b98245f09b4780054152e
Lingyu-ILY <lingyu2022352@gmail.com>
2026-01-08 16:45:57 +0800

    chore(tw): translate admin/user language strings to Traditional Chinese

--- resources/lang/en/admin/user.php
+++ resources/lang/en/admin/user.php
@@ -2,10 +2,10 @@
 
 return [
     'exceptions' => [
-        'user_has_servers' => 'Cannot delete a user with active servers attached to their account. Please delete their servers before continuing.',
+        'user_has_servers' => '無法刪除帳號下仍有啟用中伺服器的使用者。請先刪除其伺服器再繼續。',
     ],
     'notices' => [
-        'account_created' => 'Account has been created successfully.',
-        'account_updated' => 'Account has been successfully updated.',
+        'account_created' => '帳號已成功建立。',
+        'account_updated' => '帳號已成功更新。',
     ],
 ];
